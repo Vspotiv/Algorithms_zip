a60e1cb712f5d843bd7b90afb5923321e2bfb4c4
David Ilnytskyy <116728854+DavidIlnytskyi@users.noreply.github.com>
2023-03-06 16:55:51 +0200

    added 1st implementation of Huffman Code
    realised Node class, HuffmanCode class

---
+++ HuffmanCode.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, value=None, symbol=None, is_leaf=False, lc=None, rc = None):
+        self.value = value
+        self.symbol = symbol
+        self.left_children = lc
+        self.right_children = rc
+        self.is_leaf = is_leaf
+        self.code = None
+
+class Huffman_code:
+    def __init__(self, sequence):
+        self.sequence = sequence
+        self.stats = []
+        self.root = None
+
+    def calculate_probabities(self):
+        for symbola in set(self.sequence):
+            self.stats.append(Node(value = self.sequence.count(symbola), symbol=symbola, is_leaf=True))
+        self.stats = sorted(self.stats, key=lambda x: x.value, reverse=True)
+        self.leaves = self.stats
+
+    def create_tree(self):
+        while len(self.stats) != 1:
+            self.creation(self.stats[-1], self.stats[-2])
+
+    def creation(self, node_right, node_left):
+        if len(self.stats) != 2:
+            self.stats = self.stats[:-2]
+            self.stats.append(Node(value = node_left.value + node_right.value, rc = node_right, lc = node_left))
+            self.stats = sorted(self.stats, key=lambda x: x.value, reverse=True)
+        else:
+            self.root = Node(value = node_left.value + node_right.value, rc = node_right, lc = node_left)
+            self.stats = [Node(value = node_left.value + node_right.value, rc = node_right, lc = node_left)]
+
+    def get_codes(self, root, code=''):
+        try:
+            if root.is_leaf:
+                root.code = code
+            self.get_codes(root.left_children, code=code + '0')
+            self.get_codes(root.right_children, code=code + '1')
+
+        except AttributeError:
+            None
