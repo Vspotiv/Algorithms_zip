03692266f4459f4b2bc0568fb334809a0069e8fa
Vspotiv <bohdanpavlyk122@gmail.com>
2023-03-07 19:32:59 +0200

    update without errors

--- lz77.py
+++ lz77.py
@@ -1,25 +1,22 @@
 import re
-
+import time
 class LZ77:
-    def __init__(self, buffer_length = 10):
+    def __init__(self, buffer_length = 10, lookahead = 150):
         self.buffer_length = buffer_length
-        self.lookahead = 150
+        self.lookahead = lookahead
 
     def compress(self, text):
-        
         buffer = ''
         code = []
         while text:
             text, offset, length, next, buffer = self.best_matches_compress(text, buffer)
-            code.append((offset, length, next))            
+            code.append((offset, length, next))
         return code
     
     def decompress(self, code):
         buffer = ''
         massage = ''
         for i in code:
-            if len(massage) > 900:
-                print(2)
             index = len(buffer) - i[0]
             if not i[0]:
                 massage += i[2]
@@ -50,27 +47,32 @@ class LZ77:
                             length = i
                             next = data[i]
                         else:
-                            return (data[i:], offset, length, next, (buffer + data[:i])[-self.buffer_length:])
+                            break 
                     else:
-                        return (data[i:], offset, length, next, (buffer + data[:i])[-self.buffer_length:])
+                        break     
                 else:
                     if data[:i] in buffer:
-                        index = [m.start() for m in re.finditer(data[:i], buffer)][-1]
+                        index = buffer.index(data[:i])
                         repetition = buffer[index : index + i]
                         offset = len(buffer) - index
                         length = i
                         next = data[i]
                     else:
-                        return (data[i:], offset, length, next, (buffer + data[:i])[-self.buffer_length:])
-            return (data[i:], offset, length, next, (buffer + data[:i])[-self.buffer_length+1:])
+                        break
+            return (data[i:], offset, length, next, (buffer + data[:i])[-self.buffer_length:])
         except IndexError:
             next = ''
             return (data[i:], offset, length, next, (buffer + data[:i])[-self.buffer_length:])
 
 if __name__ == '__main__':
-    lz = LZ77(15)
-    with open("sample3.txt") as file:
+    lz = LZ77(20, 250)
+    with open("test_documents/sample-75kb-text-file.txt") as file:
         data = file.read()
+    start_comp = time.time()
     compressed = lz.compress(data)
+    end_comp = time.time()
+    start_decomp = time.time()
     decompressed = lz.decompress(compressed)
+    end_decomp = time.time()
     print(data == decompressed)
+    print(end_comp-start_comp, end_decomp-start_decomp)
